3ae92ebf87701b5bf2cb74aad69dc199219286e5
Chris Thomas <chris0392@gmail.com>
2017-02-16 04:25:43 +0000

    Added profiles table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161211162326) do
+ActiveRecord::Schema.define(version: 20170216041906) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
